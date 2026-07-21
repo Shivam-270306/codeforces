88e950fe66cb721a73dfccae5936638687b65599
Shivam Janwani <janwanishivam7@gmail.com>
2026-07-22 00:18:39 +0530

    Day 6

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+    Scanner sc =  new Scanner(System.in);
+
+    int test = sc.nextInt();
+
+    while(test-->0){
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        if (k > 1){
+            System.out.println("Yes");
+
+        } else{
+            
+            for(int i = 1 ; i<n ; i++ ){
+                if(arr[i] < arr[i-1]){
+                    count++;
+                }
+            }
+        
+
+        if(count > 0){
+            System.out.println("No");
+            
+        }else{
+            System.out.println("Yes");
+        }
+    }
+    }
+    }
+}
