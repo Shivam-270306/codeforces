76fbbedf3bf1c099203582b5c17e84b4be7003b7
Shivam Janwani <janwanishivam7@gmail.com>
2026-07-24 23:03:03 +0530

    Day7

--- Day7.java
+++ Day7.java
@@ -1,3 +1,18 @@
+import java.util.*;
+
 public class Day7 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        String[] arr = s.split("\\+");
+
+        Arrays.sort(arr);
+
+        System.out.print(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print("+" + arr[i]);
+        }
+    }
 }
